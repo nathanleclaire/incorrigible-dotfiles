31388109618602a89378f4aa477b5e54cdbc9572
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-05-24 13:22:02 -0700

    Add .bash_profile support to install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm -rf $HOME/.vim $HOME/.vimrc $HOME/.bashrc
+rm -rf $HOME/.vim $HOME/.vimrc $HOME/.bashrc $HOME/.bash_profile
 ln -s `pwd`/.vim $HOME/.vim
 ln -s `pwd`/.vimrc $HOME/.vimrc
 ln -s `pwd`/.bashrc $HOME/.bashrc
+ln -s `pwd`/.bash_profile $HOME/.bash_profile
 . $HOME/.bashrc
