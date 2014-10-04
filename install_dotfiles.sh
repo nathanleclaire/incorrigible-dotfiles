bbc192b9a4aca5bd82b486b472ef214396f6f9d5
Nathan LeClaire <nathan.leclaire@docker.com>
2014-10-04 13:35:14 -0700

    Remove ~/.bash_profile from VC

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,5 +4,4 @@ rm -rf $HOME/.vim $HOME/.vimrc $HOME/.bashrc $HOME/.bash_profile
 ln -s `pwd`/.vim $HOME/.vim
 ln -s `pwd`/.vimrc $HOME/.vimrc
 ln -s `pwd`/.bashrc $HOME/.bashrc
-ln -s `pwd`/.bash_profile $HOME/.bash_profile
 . $HOME/.bashrc
