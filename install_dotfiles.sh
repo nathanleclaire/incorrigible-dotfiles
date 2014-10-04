c3c355aaeca7696ad1ce51e8f6aa0d368bae3427
Nathan LeClaire <nathan.leclaire@docker.com>
2014-10-04 13:44:44 -0700

    Add emacs config to dotfiles !

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm -rf $HOME/.vim $HOME/.vimrc $HOME/.bashrc $HOME/.bash_profile
-ln -s `pwd`/.vim $HOME/.vim
-ln -s `pwd`/.vimrc $HOME/.vimrc
-ln -s `pwd`/.bashrc $HOME/.bashrc
-. $HOME/.bashrc
+rm -rf ${HOME}/.vim ${HOME}/.vimrc ${HOME}/.bashrc ${HOME}/.bash_profile
+ln -s $(pwd)/.vim ${HOME}/.vim
+ln -s $(pwd)/.vimrc ${HOME}/.vimrc
+ln -s $(pwd)/.bashrc ${HOME}/.bashrc
+ln -s $(pwd)/.emacs.d ${HOME}/.emacs.d
+. ${HOME}/.bashrc
