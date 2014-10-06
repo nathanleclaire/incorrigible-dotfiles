cdec71079d4ed15eb25981714ba2ea74ef5012db
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-10-05 22:25:42 -0700

    AUTOMATED DOTFILES UPDATE | Sun Oct  5 22:25:42 PDT 2014 zenlike@wintermute (ﾟヮﾟ)

--- install_dotfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf ${HOME}/.vim ${HOME}/.vimrc ${HOME}/.bashrc ${HOME}/.bash_profile
-ln -s $(pwd)/.vim ${HOME}/.vim
-ln -s $(pwd)/.vimrc ${HOME}/.vimrc
-ln -s $(pwd)/.bashrc ${HOME}/.bashrc
-ln -s $(pwd)/.emacs.d ${HOME}/.emacs.d
-. ${HOME}/.bashrc
